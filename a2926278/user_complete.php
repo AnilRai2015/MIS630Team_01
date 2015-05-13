545779baf9a4a47c49fdd2854b647e0cec20b3e4
pravikant52 <prao@stevens.edu>
2015-05-13 00:49:26 -0400

    Update user_complete.php

--- a2926278/user_complete.php
+++ a2926278/user_complete.php
@@ -6,43 +6,49 @@ session_start();
 <body>
 
 <?php
-$servername = "mysql13.000webhost.com";
-$username = "a2926278_mis630";
-$password = "mis630";
-$dbname = "a2926278_mis630";
-
+$servername = "mysql2.000webhost.com";
+$username = "a2185196_11";
+$password = "team11";
+$dbname = "a2185196_11";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 if(isset($_SESSION["currentuser"]))
 {
    
    $userID=(int)$_SESSION["currentuser"];
    $complete= $_GET["complete"];
-   //$serviceNo=(int)$_SESSION["serviceno"];
-   if($complete)
+   $ServiceNo=$_GET["ServiceNo"];
+   if($userID)
    {
-      $sql = "UPDATE MechanicService set Status='Completed' where UserID=$userID and Status='Processing'";
+      $sql = "UPDATE mechanicservice set Status='Completed' where UserID=$userID and Status='Processing' and ServiceNo=$ServiceNo";
  
       $result = mysqli_query($conn, $sql);
       if ($result)
       {
-        echo "Your order is completed! Thank you!";
-        echo  "<ul><li class='menuitem'><a href='user_home.html'>Go bcak</a></li></ul>";
-       }
-       else{echo "Fail to complete!";}
-    }
+        echo "<h2>Your order is completed! Payment has been processed for Service Number: $ServiceNo<br></h2>";
+        echo "<h2>Thank you!<br></h2>";
+        echo "<a href=\"javascript:history.go(-1)\">GO BACK</a>";
+       }else{
+          echo "<h2>Error: Fail to complete!, Problem with Update and DATA BASE. login Again!<br></h2>";
+          echo  "<ul><li class='menuitem'><a href='logout.php'>LogOUT Now</a></li></ul>";
+      }
+
+    }else{
+    echo "<h2>Error: Not getting Current User Information, login Again!<br></h2>";
+    echo  "<ul><li class='menuitem'><a href='logout.php'>LogOUT Now</a></li></ul>";
+      } 
   
 }
-else {
-    echo "Error!";
-}
+else{
+    echo "Error: Not getting Current User Information, login Again!";
+    echo  "<ul><li class='menuitem'><a href='logout.php'>LogOUT Now</a></li></ul>";
+      } 
 mysqli_close($conn);
 ?>  
 
 </body>
-</html>	
+</html>			
