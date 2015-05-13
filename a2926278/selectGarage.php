cc1b634657086c5b6d40f1eb33b614eb74b13c61
pravikant52 <prao@stevens.edu>
2015-05-13 00:46:51 -0400

    Update selectGarage.php

--- a2926278/selectGarage.php
+++ a2926278/selectGarage.php
@@ -6,50 +6,28 @@ session_start();
 <body>
 
 <?php
-$servername = "mysql13.000webhost.com";
-$username = "a2926278_mis630";
-$password = "mis630";
-$dbname = "a2926278_mis630";
-
+$servername = "mysql2.000webhost.com";
+$username = "a2185196_11";
+$password = "team11";
+$dbname = "a2185196_11";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 $Area = $_GET["Area"];
-
-$sql = "SELECT Area, GarageID,Time,Distance From CoveringArea where Area='$Area' ";
-
-$sql2 = "SELECT Area, GarageID,Time,Distance From CoveringArea where Area='$Area' ";
-
+$sql = "SELECT Area, GarageID,Time,Distance From coveringArea where Area='$Area' ";
 $result = mysqli_query($conn, $sql);
-$result2 = mysqli_query($conn, $sql);
 
-
-if (mysqli_num_rows($result) > 0 and mysqli_num_rows($result2)) { 
-    echo "<form action='createorder.php' target='content_main'>";
-    echo "Choose One Garage:<br>";
-    echo "<select name='selection'>";
-    while($row2 = mysqli_fetch_assoc($result2)) 
-    {
-      $garageID=(string)$row2["GarageID"];
-      echo "<option value=$garageID>".$row2["GarageID"]."</option>";
-     }
-    echo "</select>";
-    echo "<br><br>";
-    echo "ProblemDescription:<br>";
-    echo "<input type='text' name='problem'>";
-    echo "<input type='submit'>";
-    echo "</form>";
+if (mysqli_num_rows($result) > 0) { 
     echo '<div id="content_main">';
     echo "<table border=0>";
     echo "<tr> <th> Area </th> <th> GarageID </th><th> Time </th><th> Distance(miles) </th><th></th></tr>";
-    while($row = mysqli_fetch_assoc($result)) 
-    {    
+    while($row = mysqli_fetch_assoc($result)) {
         echo "<tr>";
-        echo "<td>". $row["Area"]. "</td><td>" . $row["GarageID"]. "</td><td>" . $row["Time"]."</td><td>". $row["Distance"]."</td>";
+$_SESSION["chosegarage"]=$row["GarageID"];
+        echo "<td>". $row["Area"]. "</td><td>" . $row["GarageID"]. "</td><td>" . $row["Time"]."</td><td>". $row["Distance"]."</td><td><form action='enter_info.html' target='content_main'><button>Choose<button></form></td>";
         echo "</tr>";
      }
    echo "</table>";
@@ -62,4 +40,4 @@ mysqli_close($conn);
 ?>  
 
 </body>
-</html>	
+</html>			
