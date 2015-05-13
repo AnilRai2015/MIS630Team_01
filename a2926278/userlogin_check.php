7af534e06df0ddd36d68ae1ea12cebdb3462a062
pravikant52 <prao@stevens.edu>
2015-05-13 00:52:39 -0400

    Update userlogin_check.php

--- a2926278/userlogin_check.php
+++ a2926278/userlogin_check.php
@@ -1,50 +1,54 @@
 <?php
-session_start();
+	session_start();
 ?>
 <!DOCTYPE html>
 <html>
 <body>
 
 <?php
-$servername = "mysql13.000webhost.com";
-$username = "a2926278_mis630";
-$password = "mis630";
-$dbname = "a2926278_mis630";
-
+$servername = "mysql2.000webhost.com";
+$username = "a2185196_11";
+$passwordDB = "team11";
+$dbname = "a2185196_11";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $passwordDB, $dbname);
 // Check connection
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+	die("Connection failed: ".mysqli_connect_error());
 }
 $id = $_GET["login"];
-$password = $_GET["password"];
-
-if($id)
-{
-
-$_SESSION["currentuser"]=$id;
-
-$sql = "SELECT UserID,Password FROM Users where UserID=$id";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_row($result);
-
-   if (mysqli_num_rows($result) > 0) 
-   {
-       echo $row[1];
-       if ($row[1] == $password)
-       {header("Location: user_home.html");}
-      else{echo "invalid password";}
-    }
-   else 
-   {
-    echo "Doesn't exist!";
-   }
-} 
-
-
+$passwordUser = $_GET["password"];
+ if ($id){
+	$_SESSION["currentuser"] = $id;
+	$sql = "SELECT UserID,Password FROM Users where UserID=$id";
+	$result = mysqli_query($conn, $sql);
+	   if ($result){
+		$row = mysqli_fetch_row($result);
+		if (mysqli_num_rows($result) > 0)
+		{
+			//echo $row[1];
+                        //echo $passwordUser ;
+			if ($passwordUser == $row[1])
+			{
+				header("Location: user_home.html");
+			}else{
+                               echo "<h2>Password does Not MATCH!</h2>";
+		               echo  "<ul><li class='menuitem'><a href='user_login.html'>Go back</a></li></ul>";
+                              }
+		}else{
+                        echo "<h2>Doesn't exist!!</h2>";
+                        echo  "<ul><li class='menuitem'><a href='user_login.html'>Go back</a></li></ul>";
+		}
+	    }else{
+		echo "<h2>User Not Found!</h2>";
+		echo  "<ul><li class='menuitem'><a href='user_login.html'>Go back</a></li></ul>";
+	}
+ }else{
+echo "<h2>User_ID can't be blank!</h2>";
+		echo  "<ul><li class='menuitem'><a href='user_login.html'>Go back</a></li></ul>";
+      }
 mysqli_close($conn);
-?>  
+?>
 
 </body>
-</html>	
+</html>		
