d3392491c98ebcf679b7c594f3407422c5fbb7ee
pravikant52 <prao@stevens.edu>
2015-05-13 00:42:12 -0400

    Update garage_pricing.php

--- a2926278/garage_pricing.php
+++ a2926278/garage_pricing.php
@@ -6,32 +6,32 @@ session_start();
 <body>
 
 <?php
-$servername = "mysql13.000webhost.com";
-$username = "a2926278_mis630";
-$password = "mis630";
-$dbname = "a2926278_mis630";
-
+$servername = "mysql2.000webhost.com";
+$username = "a2185196_11";
+$password = "team11";
+$dbname = "a2185196_11";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 if(isset($_SESSION["currentgarage"]))
 {
    
    $garageID=(int)$_SESSION["currentgarage"];
    $price= $_GET["price"];
+   $ServiceNo= $_GET["serviceNum"];
    //$serviceNo=(int)$_SESSION["serviceno"];
    
-   $sql = "UPDATE MechanicService set Price=$price, Status='Granted' where GarageID=$garageID and Status='Waiting'";
+   $sql = "UPDATE mechanicservice set Price=$price, Status='Granted' where GarageID=$garageID and Status='Waiting' and ServiceNo=$ServiceNo";
  
    $result = mysqli_query($conn, $sql);
    if ($result)
    {
-     echo "Successfully price it!";
-     echo  "<ul><li class='menuitem'><a href='garage_home.html'>Go bcak</a></li></ul>";
+     echo "<h2>Successfully price it!<br></h2>";
+echo "<h3>You priced to Service Number: $ServiceNo</h3>";
+     echo  "<ul><li class='menuitem'><a href='garage_home.html'>Go back</a></li></ul>";
    }
    else{echo "Fail to price it!";}
 }
